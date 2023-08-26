c573c5bc12e64a58b6db19676331f54afdc6113f
Satya Ranjon <satyaranjonofficial@gmail.com>
2023-08-26 10:38:06 +0600

    complite Invite Send component design part

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,12 +2,14 @@ import Logo from "../assets/images/logo.svg";
 import Login from "../assets/images/login.svg";
 import Registration from "../assets/images/registration.svg";
 import notfound from "../assets/images/notfound.png";
+import inviteSend from "../assets/images/inviteSend.svg";
 
 const images = {
   Logo,
   Login,
   Registration,
   notfound,
+  inviteSend,
 };
 
 export default images;
