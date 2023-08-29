f61b944e5d0ffbe9e71bc86cbd5292fad73b453d
Satya Ranjon <satyaranjonofficial@gmail.com>
2023-08-29 15:19:03 +0600

    Update the LoadingButton Component , resive one more props disabled

--- src/components/common/LoadingButton.jsx
+++ src/components/common/LoadingButton.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import LoadingIcon from "./LoadingIcon";
 
-const LoadingButton = ({ isLoading = false, children, ...argument }) => {
+const LoadingButton = ({
+  isLoading = false,
+  disabled = false,
+  children,
+  ...argument
+}) => {
   return (
     <button
+      disabled={disabled}
       {...argument}
-      className="text-lg bg-black text-white rounded-sm p-2 px-8">
+      className={`text-lg bg-black text-white flex justify-center items-center rounded-sm p-2 px-8 ${
+        disabled && "opacity-50"
+      }`}>
       {isLoading && <LoadingIcon />}
       {children}
     </button>
