517f863fd0fd99318a34ad78dc9bd8799a6d6b8b
Satya Ranjon <satyaranjonofficial@gmail.com>
2023-08-29 15:53:56 +0600

    Add new component SelectTime , this component Select Time hour or minute using TimePicker component and this component resive three props lable,initialState,getValue , this getValue is callback function this function give selected Time object formet {hour:0,minute:0}

--- src/pages/event/addevent/SelectTime.jsx
+++ src/pages/event/addevent/SelectTime.jsx
@@ -1,14 +1,9 @@
 import { useRef, useState } from "react";
 import { CiTimer, CiViewTimeline } from "react-icons/ci";
 import useOutsideClick from "../../../hooks/useOutsideClick";
-import MinutePicker from "../../../components/minutePicker/MinutePicker";
+import TimePicker from "../../../components/TimePicker/TimePicker";
 
-const initialState = {
-  hour: "00",
-  minute: "00",
-};
-
-const SelectTime = () => {
+const SelectTime = ({ label = "", initialState, getValue = () => {} }) => {
   const [openTimePicker, setOpenTimePicker] = useState(false);
   const [selectedTime, setSelectedTime] = useState(initialState);
 
@@ -25,8 +20,8 @@ const SelectTime = () => {
 
   const selectedDateHandler = (date) => {
     setSelectedTime(date);
+    getValue(date);
   };
-  console.log(selectedTime);
 
   return (
     <div
@@ -34,18 +29,18 @@ const SelectTime = () => {
       className="flex justify-start items-center w-full text-dark-light text-sm mt-2 z-10">
       <div className="w-[25%] flex justify-start items-center gap-2 p-1 text-lg ">
         <CiTimer />
-        <span className=" text-sm">Start Time</span>
+        <span className=" text-sm">{label}</span>
       </div>
       <div className="w-[75%] flex justify-start items-center gap-3 relative">
         <span className=" p-1 px-2 bg-hover cursor-pointer ">
           <span>
-            {selectedTime.hour < 9
-              ? `0${selectedTime.hour}`
-              : selectedTime.hour}
+            {selectedTime?.hour < 9
+              ? `0${selectedTime?.hour}`
+              : selectedTime?.hour}
             :
-            {selectedTime.minute < 9
-              ? `0${selectedTime.minute}`
-              : selectedTime.minute}
+            {selectedTime?.minute < 9
+              ? `0${selectedTime?.minute}`
+              : selectedTime?.minute}
           </span>
         </span>
         <div
@@ -53,7 +48,12 @@ const SelectTime = () => {
           className=" p-1 hover:bg-hover  cursor-pointer text-xl">
           <CiViewTimeline />
         </div>
-        {openTimePicker && <MinutePicker getValue={selectedDateHandler} />}
+        {openTimePicker && (
+          <TimePicker
+            getValue={selectedDateHandler}
+            initialState={initialState}
+          />
+        )}
       </div>
     </div>
   );
