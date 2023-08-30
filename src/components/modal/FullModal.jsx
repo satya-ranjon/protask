f338e2d9c4741ecbe447ebb775eaf1e4820b000c
Satya Ranjon <satyaranjonofficial@gmail.com>
2023-08-30 11:06:58 +0600

    update FullModal and Modal component add this component farmer-motion animation

--- src/components/modal/FullModal.jsx
+++ src/components/modal/FullModal.jsx
@@ -1,22 +1,44 @@
 import { GrClose } from "react-icons/gr";
+import { motion, AnimatePresence } from "framer-motion";
 
 const FullModal = ({ isOpen, children, onClose }) => {
-  if (!isOpen) return null;
+  const modalVariants = {
+    hidden: {
+      opacity: 0,
+      x: "100%",
+    },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: 0.3,
+      },
+    },
+  };
   return (
-    <div className="fixed -right-31 top-0  bg-[#ffffff] w-full h-full modal ">
-      <div className="w-full flex sm:gap-5 justify-end items-start sm:pr-28">
-        <div className="w-full h-screen px-3 sm:px-0">{children}</div>
-        <div className=" sm:w-5 xl:w-10 sm:h-screen bg-hover "></div>
-        <div className=" sm:w-8 xl:w-16 sm:h-screen bg-hover "></div>
-        <div className="absolute top-0 right-0 sm:static  w-24 sm:w-24 xl:w-36 h-28 sm:h-screen bg-hover rounded-es-[80px] sm:rounded-es-none">
-          <div
-            className="border-2 w-10 h-10 flex justify-center items-center mx-auto mt-5 sm:mt-12 text-gray-900 border-gray-400 cursor-pointer"
-            onClick={onClose}>
-            <GrClose />
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div
+          className="fixed -right-31 top-0  bg-[#ffffff] w-full h-full modal "
+          initial="hidden"
+          animate="visible"
+          exit="hidden"
+          variants={modalVariants}>
+          <div className="w-full flex sm:gap-5 justify-end items-start sm:pr-28">
+            <div className="w-full h-screen px-3 sm:px-0">{children}</div>
+            <div className=" sm:w-5 xl:w-10 sm:h-screen bg-hover "></div>
+            <div className=" sm:w-8 xl:w-16 sm:h-screen bg-hover "></div>
+            <div className="absolute top-0 right-0 sm:static  w-24 sm:w-24 xl:w-36 h-28 sm:h-screen bg-hover rounded-es-[80px] sm:rounded-es-none">
+              <div
+                className="border-2 w-10 h-10 flex justify-center items-center mx-auto mt-5 sm:mt-12 text-gray-900 border-gray-400 cursor-pointer"
+                onClick={onClose}>
+                <GrClose />
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
 
--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -2,29 +2,51 @@ import ReactDOM from "react-dom";
 import { MdOpenInFull } from "react-icons/md";
 import { AiOutlineClose } from "react-icons/ai";
 import ButtonIcon from "../common/ButtonIcon";
+import { motion, AnimatePresence } from "framer-motion";
+
 const Modal = ({ isOpen = false, onClose, children, cls, fullPage }) => {
-  if (!isOpen) return null;
+  const modalVariants = {
+    hidden: {
+      opacity: 0,
+      y: "-100%",
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.3,
+      },
+    },
+  };
 
   return ReactDOM.createPortal(
-    <div className="w-full  h-full fixed top-0 left-0 overflow-hidden flex justify-center items-center bg-[#000000c4]">
-      <div
-        className={`absolute w-[90%]  lg:w-[60%] bg-white  p-1 border-gray-500  shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px]  ${cls} `}>
-        <div className=" flex justify-between">
-          {fullPage ? (
-            <ButtonIcon onClick={fullPage} tooltipLeft="Open in full page">
-              <MdOpenInFull />
-            </ButtonIcon>
-          ) : (
-            <div className=""></div>
-          )}
-          <ButtonIcon onClick={onClose} tooltipRight="Close">
-            <AiOutlineClose />
-          </ButtonIcon>
-        </div>
+    <AnimatePresence>
+      {isOpen && (
+        <div className="w-full  h-full fixed top-0 left-0 overflow-hidden flex justify-center items-center bg-[#000000c4]">
+          <motion.div
+            className={`absolute w-[90%]  lg:w-[60%] bg-white  p-1 border-gray-500  shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px]  ${cls} `}
+            initial="hidden"
+            animate="visible"
+            exit="hidden"
+            variants={modalVariants}>
+            <div className=" flex justify-between">
+              {fullPage ? (
+                <ButtonIcon onClick={fullPage} tooltipLeft="Open in full page">
+                  <MdOpenInFull />
+                </ButtonIcon>
+              ) : (
+                <div className=""></div>
+              )}
+              <ButtonIcon onClick={onClose} tooltipRight="Close">
+                <AiOutlineClose />
+              </ButtonIcon>
+            </div>
 
-        <div>{children}</div>
-      </div>
-    </div>,
+            <div>{children}</div>
+          </motion.div>
+        </div>
+      )}
+    </AnimatePresence>,
     document.body
   );
 };
